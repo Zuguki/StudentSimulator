93af6f2c7194e76347a20b6424c30fe600a58fbb
Zuguki <guskov.zahar20@gmail.com>
2022-04-23 13:49:35 +0500

    Meet prototype was added

---
+++ Assets/Scripts/Meet.cs
@@ -0,0 +1,10 @@
+﻿namespace DefaultNamespace
+{
+    public abstract class Meet : IStatButton
+    {
+        public string ButtonText { get; }
+        
+        public void ButtonFunc()
+        { }
+    }
+}
