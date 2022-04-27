dd1cdb49e834fb67a52342e3f34fb475c9da5571
Zuguki <guskov.zahar20@gmail.com>
2022-04-27 17:56:09 +0500

    ButtonsLoader was changed

--- Assets/Scripts/ButtonsLoader.cs
+++ Assets/Scripts/ButtonsLoader.cs
@@ -14,9 +14,10 @@ public class ButtonsLoader : MonoBehaviour
 
    private List<GameObject> _items = new();
    private VerticalLayoutGroup _group;
-   private List<IStatButton> _stats = new()
+
+   private readonly List<IStatButton> _stats = new()
    {
-      new EventFromUniversity(), new MentorGroupChats(), 
+      new EventFromUniversity(), new MentorGroupChats(),
       new AskForNotesFromClassmates(), new EngageInVideoCourse()
    };
 
@@ -30,37 +31,32 @@ public class ButtonsLoader : MonoBehaviour
    private void SetTitles(Component rectT)
    {
       var currentState = _stats.Where(stat => stat.StateType == statType).ToList();
+      var titles = currentState.Select(btn => btn.Text);
       rectT.transform.localPosition = new Vector3(0f, 0f, 0f);
-<<<<<<< HEAD
-      if (titles.Length <= 0)
-=======
 
-      if (currentState.Count <= 0) 
->>>>>>> dev
+      if (currentState.Count <= 0)
          return;
-      
+
       var prefabButton = Instantiate(buttonPrefab, transform);
       var height = prefabButton.GetComponent<RectTransform>().rect.height;
       var rectTransform = GetComponent<RectTransform>();
-<<<<<<< HEAD
-      rectTransform.sizeDelta = new Vector2(rectTransform.rect.width, height * titles.Length);
-         
+      rectTransform.sizeDelta = new Vector2(rectTransform.rect.width, height * currentState.Count);
+
       Destroy(prefabButton);
       foreach (var title in titles)
       {
          var prefab = Instantiate(buttonPrefab, transform);
          prefab.GetComponentInChildren<Text>().text = title;
-=======
-         
-      rectTransform.sizeDelta = new Vector2(rectTransform.rect.width, height * currentState.Count);
-         
-      Destroy(prefabButton);
-      foreach (var statButton in currentState)
-      {
-         var prefab = Instantiate(buttonPrefab, transform);
-         prefab.GetComponentInChildren<Text>().text = statButton.Text;
->>>>>>> dev
-         _items.Add(prefab);
+
+         rectTransform.sizeDelta = new Vector2(rectTransform.rect.width, height * currentState.Count);
+
+         Destroy(prefabButton);
+         foreach (var statButton in currentState)
+         {
+            prefab = Instantiate(buttonPrefab, transform);
+            prefab.GetComponentInChildren<Text>().text = statButton.Text;
+            _items.Add(prefab);
+         }
       }
    }
 }
