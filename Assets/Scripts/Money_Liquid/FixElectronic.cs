0f837d8d97f2a729d24f683255d33cd22ae0fd25
Zuguki <guskov.zahar20@gmail.com>
2022-05-02 00:17:01 +0500

    Money stats done'

--- Assets/Scripts/Money_Liquid/FixElectronic.cs
+++ Assets/Scripts/Money_Liquid/FixElectronic.cs
@@ -1,10 +1,58 @@
-﻿namespace DefaultNamespace.Money
+﻿using UnityEngine;
+
+namespace DefaultNamespace.Money
 {
     public class FixElectronic : IStatButton
     {
         public StatType StateType => StatType.Money;
         public string Text => "Починить подик знакомому";
+
+        private const int NeedsMeet = 1000;
+        private const int NeedsScience = 1000;
+
+        private int _meet;
+        private int _science;
+        private int _respect;
+        private int _money;
+
         public void Buffs()
-        { }
+        {
+            _meet = PlayerPrefs.GetInt("meet");
+            _science = PlayerPrefs.GetInt("science");
+            _respect = PlayerPrefs.GetInt("respect");
+            _money = PlayerPrefs.GetInt("money");
+
+            if (TryGetGoodBuff(out var buffValue))
+            {
+                Debug.Log("О, теперь мы делаем бизнес");
+                _money += buffValue;
+            }
+            else
+            {
+                Debug.Log("Так, ну тут, либо заказов нет, либо знаний нет, либо срукожопил где то");
+                _meet -= _meet - buffValue > 0 ? buffValue : _meet;
+                _science -= _science - buffValue > 0 ? buffValue : _science;
+                _respect -= _respect - buffValue > 0 ? buffValue : _respect;
+            }
+            
+            UpdatePrefabValue();
+        }
+        
+        private bool TryGetGoodBuff(out int buffValue)
+        {
+            var isGoodBuff = _meet > NeedsMeet && _science > NeedsScience && Random.Range(0, 100) > 10;
+            
+            buffValue = Random.Range(100, 500);
+            return isGoodBuff;
+        }
+        
+        private void UpdatePrefabValue()
+        {
+            PlayerPrefs.SetInt("meet", _meet);
+            PlayerPrefs.SetInt("science", _science);
+            PlayerPrefs.SetInt("respect", _respect);
+            PlayerPrefs.SetInt("money", _money);
+            PlayerStats.NeedsUpdate = true;
+        }
     }
 }
--- Assets/Scripts/Money_Liquid/SellConspects.cs
+++ Assets/Scripts/Money_Liquid/SellConspects.cs
@@ -1,10 +1,48 @@
-﻿namespace DefaultNamespace.Money
+﻿using UnityEngine;
+
+namespace DefaultNamespace.Money
 {
     public class SellConspects : IStatButton
     {
         public StatType StateType => StatType.Money;
         public string Text => "Продать конспекты";
+
+        private const int NeedsScience = 3000;
+
+        private int _science;
+        private int _money;
+
         public void Buffs()
-        { }
+        {
+            _science = PlayerPrefs.GetInt("science");
+            _money = PlayerPrefs.GetInt("money");
+
+            if (TryGetGoodBuff(out var buffValue))
+            {
+                Debug.Log("Так, конаспект продали, бабки получили, все З&С");
+                _money += buffValue;
+            }
+            else
+            {
+                Debug.Log($"Ну, либо знаний маловато, нужно: {NeedsScience}, или не повезло");
+            }
+            
+            UpdatePrefabValue();
+        }
+        
+        private bool TryGetGoodBuff(out int buffValue)
+        {
+            var isGoodBuff = _science > NeedsScience && Random.Range(0, 100) > 10;
+            
+            buffValue = Random.Range(200, 600);
+            return isGoodBuff;
+        }
+        
+        private void UpdatePrefabValue()
+        {
+            PlayerPrefs.SetInt("science", _science);
+            PlayerPrefs.SetInt("money", _money);
+            PlayerStats.NeedsUpdate = true;
+        }
     }
 }
